c1db26b305cda2738fa5963f27649bd3f381a1a5
Azra <yukselazrael@gmail.com>
2023-11-07 16:16:18 +0300

    7 kasım çalışma

---
+++ reactkurs711/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+// import img1 from '../src/img.jpg';
+
+
+function App() {
+  return (
+    <div>
+      <div className="header1">
+
+        <div>
+<i className="fa-solid fa-otter" ></i>
+        </div>
+        <div>
+          <ul className="headerMenu1">
+            <li className="menuItem1">
+              <a href="#">Main Page</a>
+            </li>
+            <li className="menuItem1">
+              <a href="#">About Me</a>
+            </li>
+            <li className="menuItem1">
+              <a href="#">Targets</a>
+            </li>
+            <li className="menuItem1">
+              <a href="#">Portfolio</a>
+            </li>
+            <li className="menuItem1">
+              <a href="#">Contacts</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
